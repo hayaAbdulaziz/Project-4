63aa3310f5b2b7b0d0a6931e3dbc83b88a68b1d0
hayaAbdulaziz <hayal5066@gmail.com>
2020-04-03 16:56:01 +0300

    creat New Exercise

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,7 +2,30 @@ class ExercisesController < ApplicationController
     def index
         @exercises = Exercise.all  
     end
+    
     def show
         @exercise = Exercise.find(params[:id])
+      end
+
+      def new
+        @exercise = Exercise.new
+      end
+
+      def edit
+        @exercise = Exercise.find(params[:id])
+      end
+
+      def create
+        @exercise = Exercise.new(exercise_params)
+    
+        @exercise.save
+    
+        redirect_to @exercise
+      end
+
+      private
+    
+      def exercise_params
+        params.require(:exercise).permit(:name, :explain, :img)
       end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'exercises#index'
   resources :exercises
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
