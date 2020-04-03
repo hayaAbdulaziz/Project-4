bed9697efc6bbf90db93a50b66e7a7d42939d3da
hayaAbdulaziz <hayal5066@gmail.com>
2020-04-03 17:14:48 +0300

    creat Update Exercise

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,7 +2,7 @@ class ExercisesController < ApplicationController
     def index
         @exercises = Exercise.all  
     end
-    
+
     def show
         @exercise = Exercise.find(params[:id])
       end
@@ -14,7 +14,7 @@ class ExercisesController < ApplicationController
       def edit
         @exercise = Exercise.find(params[:id])
       end
-
+      
       def create
         @exercise = Exercise.new(exercise_params)
     
@@ -23,8 +23,17 @@ class ExercisesController < ApplicationController
         redirect_to @exercise
       end
 
+     def update
+        @exercise = Exercise.find(params[:id])
+
+        @exercise.update(exercise_params)
+
+        redirect_to  @exercise
+        
+        end
+
       private
-    
+
       def exercise_params
         params.require(:exercise).permit(:name, :explain, :img)
       end
