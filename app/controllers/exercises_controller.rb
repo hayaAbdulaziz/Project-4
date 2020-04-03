3c67d096219a1adeb38353bfea32e0615680370f
hayaAbdulaziz <hayal5066@gmail.com>
2020-04-03 14:16:43 +0300

    created for exercises and control With index and show

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,8 @@
+class ExercisesController < ApplicationController
+    def index
+        @exercises = Exercise.all  
+    end
+    def show
+        @exercise = Exercise.find(params[:id])
+      end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Exercise.create(name: "Core",  img: "https://redefiningstrength.com/wp-content/uploads/2015/03/crunchless-core-exercises.jpg")
+Exercise.create(name: "Cardio",  img: "https://787300.smushcdn.com/1494239/wp-content/uploads/2014/03/lunge-matrix-e1417976689565.jpg?lossy=0&strip=1&webp=1")
+Exercise.create(name: "Flexibilityexercises", img: "https://i.pinimg.com/564x/c1/38/46/c13846a7b936cb976cabdca75a61e777.jpg")
+Exercise.create(name: "push up", img: "https://media1.popsugar-assets.com/files/thumbor/g3IqvrBcCL8PCn2kV4oYF5LCXy0/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/11/21/628/n/1922729/0e7e9800cb65fd44_Tricep-Push-Up.jpg")
