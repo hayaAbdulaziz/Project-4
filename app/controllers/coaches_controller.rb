c7a718f7fa2440a39a2521617b69e488726a5276
hayaAbdulaziz <hayal5066@gmail.com>
2020-04-05 14:45:21 +0300

    Finished Coaches Controller With full CRUD

---
+++ app/controllers/coaches_controller.rb
@@ -0,0 +1,45 @@
+class CoachesController < ApplicationController
+  def index
+    @coaches = Coach.all  
+end
+
+def show 
+  @coach = Coach.find(params[:id])
+end 
+
+def new
+  @coach = Coach.new
+end
+
+def edit
+  @coach = Coach.find(params[:id])
+end
+
+def create
+
+@coach = Coach.new(coach_params)
+@coach.save
+redirect_to @coach
+
+end
+def update
+  @coach = Coach.find(params[:id])
+
+  @coach.update(coach_params)
+
+  redirect_to  @coach
+  
+  end
+
+def destroy
+  @coach = Coach.find(params[:id]).delete
+
+  redirect_to coaches_path
+end
+
+private
+
+def coach_params
+  params.require(:coach).permit(:name, :specialty)
+end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_173833) do
+ActiveRecord::Schema.define(version: 2020_04_05_102316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
