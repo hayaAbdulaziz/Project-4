90186b3d5d6aa38edc686355f10ee7027b281992
hayaAbdulaziz <hayal5066@gmail.com>
2020-04-08 17:48:38 +0300

    Update

---
+++ db/migrate/20200403102606_create_exercises.rb
@@ -0,0 +1,12 @@
+class CreateExercises < ActiveRecord::Migration[5.2]
+  def change
+    create_table :exercises do |t|
+      t.string :name
+      t.string :explain
+      t.string :img
+      # t.belongs_to :user
+      # t.belongs_to :coach
+      t.timestamps
+    end
+  end
+end
